75bda38dd84d5ea1ae16ecac035a6e605864c856
visvesvaran62 <visvesvaran62@gmail.com>
2026-02-26 15:10:08 +0530

    day1

--- looptask.js
+++ looptask.js
@@ -1,19 +1,29 @@
 // day-1 loop task
 
-let arr1=[1,2,3,4,5]
-for(let i=0;i<arr1.length;i++){
-    console.log("the numbers are:",arr1[i]);   
-}
-// 
-let num=[1,2,3,4,5,6,7,8,9,10];
-for(let i=0;i<num.length;i++){
-    if(num[i]% 2===0){
-     console.log("even number :",num[i]);
-    }
-}
-for(let i=1;i<=5;i++){
+// let arr1=[1,2,3,4,5]
+// for(let i=0;i<arr1.length;i++){
+//     console.log("the numbers are:",arr1[i]);   
+// }
+// // 
+// let num=[1,2,3,4,5,6,7,8,9,10];
+// for(let i=0;i<num.length;i++){
+//     if(num[i]% 2===0){
+//      console.log("even number :",num[i]);
+//     }
+// }
 
+
+// DAY-------2
+let ar1=[1,2,3,4,5]
+let sumvalue=""
+for(let i=1;i<ar1.length;i++){
+
+  sumvalue+=
 }
+
+
+
+
 
 
 
@@ -39,6 +49,27 @@ const data = outer()
 // console.log(data());
 // // console.log(data());
 
-count = 12
+// count = 12
 
-console.log(count);
+// console.log(count);
+
+
+
+
+
+
+
+let  arr=[1,2,3,4,5,6,7]
+console.log(arr);
+const dat=arr.map((e)=>e*e)
+console.log(dat)
+
+
+let arr11=[100,1000,2,6,7,8,3,5,8,10,]
+ const datat=arr11.reverse()
+console.log(datat);
+
+
+
+
+
