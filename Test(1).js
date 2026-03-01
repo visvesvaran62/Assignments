15a546d1ededf2aae908304e92f47e94a51fd443
visvesvaran62 <visvesvaran62@gmail.com>
2026-03-01 22:51:40 +0530

    test upadted

---
+++ Test(1).js
@@ -0,0 +1,43 @@
+// //  What is the difference between == and === in JavaScript?
+
+// // == ---> this is used to check equality of two values after automatically type conversion and it return boolean values.
+
+// // ===  ---->check two values equality without typical conversion and it return boolean value.
+
+
+// // // What is the difference between var, let, and const?
+
+// // var is used to decalre a variable in js.it is function scope.not block scope
+
+// // let is block scoped.let is use in when the value  will change later. 
+
+// // const is blocked scope and mostly when the value should not be reassigned.
+
+// // //  What is hoisting in JavaScript?
+// // hoisting is a java script behaivour where the variable and function move to top of their scope during execution.
+
+// // //  What is closure? Give one real-time use case.
+// // closure is when the outer execution even the inner function still remember the value.
+
+// // // What is the difference between map() and forEach()?
+// // map function used for transform and it will create new array.
+
+// // for each is do something for each item.
+
+// // What is the difference between null and undefined?
+
+// undefined is implicit value.the system through that value if not find out the value.
+// null is a value assigned intentionally by the programmer.
+
+
+//  What is the difference between shallow copy and deep copy?
+//  shallow copy its duplicte a first properties.if nested values are change the orginal value will affected.
+// deep copy create completting independent copy.modifying the deep copy doesnt affect orginal.
+
+// //  What is callback function? Why is it used?
+
+// call back function is one function argument passed as another function and execute later.
+
+// // What is the difference between arrow function and normal function?
+// normal functions have their own this and can be used as constructors.
+// // Arrow functions have lexical this and shorter syntax.
